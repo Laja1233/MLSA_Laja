8c1fdec2a7663a3cf64fb54cca57bb16609d818e
L <108204458+Laja1233@users.noreply.github.com>
2024-01-20 15:12:06 +0000

    commit alll

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Omolaja Emmanuel",
+  title: "Full Stack Developer",
+  email: "emmanuelomolaja2@gmail.com",
+  gitHub: "https://github.com/Laja1233",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/emmanuel-omolaja-1a300a242",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
